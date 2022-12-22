132ceb368fc3ad412ef772b8622993d06b3413ff
Vinay <vinaysarda2812@gmail.com>
2022-12-22 23:41:57 +0530

    Deploy

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv"
-import mongoose from "mongoose";
+import mongoose from "mongoose"
+import * as path from 'path';
+import { join , dirname} from "path";
 
 import AuthRoute from "./routes/AuthRoute.js"
 import GameRoute from "./routes/GameRoute.js"
+import { fileURLToPath } from "url";
+
 
 dotenv.config()
 const app = express();
@@ -23,6 +27,14 @@ mongoose.connect(process.env.MONGODB, ()=>{
 // Routes
 app.use("/auth", AuthRoute);
 app.use("/game", GameRoute);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(join(__dirname, "/client/build")));
+
+app.get("*", (req, res)=>{
+    res.sendFile(join(__dirname, "/client/build", "index.html"));
+});
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Listening on port ${process.env.PORT}`)
