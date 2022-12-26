fc43b13c27eba3c0da678a13dc4eccdf0abb4e2a
Vinay <vinaysarda2812@gmail.com>
2022-12-26 12:13:24 +0530

    Deploy

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if(process.env.NODE_ENV){
     app.use(express.static(path.join(__dirname, path.join("client", "build"))));
 
     app.get("*", (req, res)=>{
-        res.sendFile(path.join(__dirname, path.join(path.join("client", "index.html"), "index.html")));
+        res.sendFile(path.join(__dirname, path.join(path.join("client", "build"), "index.html")));
     });
 }
 
