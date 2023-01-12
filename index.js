d43c9838ba0c89fcd866effdbb28b709749aa82c
Vinay <vinaysarda2812@gmail.com>
2023-01-13 01:45:04 +0530

    socket.io server created

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ import * as path from 'path';
 import AuthRoute from "./routes/AuthRoute.js"
 import GameRoute from "./routes/GameRoute.js"
 import * as url from 'url';
-// import { createServer } from "http"
-// import { Server } from "socket.io";
+import { createServer } from "http"
+import { Server } from "socket.io";
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 dotenv.config()
 const app = express();
-// const server = createServer(app);
+const server = createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -23,25 +23,25 @@ app.use(cors({
 
 //socket.io set up
 
-// const io = new Server(server, {
-//     cors: {
-//         origin: "*"
-//     },
-// });
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    },
+});
 
-// io.on('connection', function (socket){
-//     // socket.on("connect_error", (err)=>console.log(err))
-//     socket.on("join_room", (gameId)=>{
-//         socket.join(gameId);
-//     })
-//     socket.on("update_game", (gameId)=>{
-//         console.log("update started");
-//         setTimeout(() => {
-//             socket.emit("start_game_update");
-//         }, 2000);
+io.on('connection', function (socket){
+    // socket.on("connect_error", (err)=>console.log(err))
+    socket.on("join_room", (gameId)=>{
+        socket.join(gameId);
+    })
+    socket.on("update_game", (gameId)=>{
+        console.log("update started");
+        setTimeout(() => {
+            socket.emit("start_game_update");
+        }, 2000);
         
-//     })
-// });
+    })
+});
 
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGODB || "mongodb+srv://vinay:brockvs28@cluster0.oldoz.mongodb.net/tic-tac-toe", ()=>{
@@ -52,7 +52,7 @@ mongoose.connect(process.env.MONGODB || "mongodb+srv://vinay:brockvs28@cluster0.
 app.use("/auth", AuthRoute);
 app.use("/game", GameRoute);
 
-app.listen(process.env.PORT || 5000, ()=>{
+server.listen(process.env.PORT || 5000, ()=>{
     console.log(`Listening on port ${process.env.PORT || 5000}`);
 })
 
