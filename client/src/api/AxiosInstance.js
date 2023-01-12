b1231ece9a162e59b368b82460d4dabd22ddec76
Vinay <vinaysarda2812@gmail.com>
2023-01-13 01:34:33 +0530

    Updated

--- client/src/api/AxiosInstance.js
+++ client/src/api/AxiosInstance.js
@@ -1,4 +1,2 @@
 import axios from "axios"
-export const API = axios.create({baseURL: "http://localhost:5000"});
-// export const API = axios.create({baseURL: "https://tic-tac-toe-ubgb.onrender.com"});
-// export const API = axios.create({baseURL: "https://tic-tac-toe-by-vinay.vercel.app/"});
+export const API = axios.create({baseURL: process.env.REACT_APP_AXIOS_BASE_URL || "http://localhost:5000"});
