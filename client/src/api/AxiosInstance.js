7f603ddbca8015bcf95344f9ba6597c6fed422d3
Vinay <vinaysarda2812@gmail.com>
2023-01-13 19:33:50 +0530

    socket.io for deployment

--- client/src/api/AxiosInstance.js
+++ client/src/api/AxiosInstance.js
@@ -1,4 +1,4 @@
 import axios from "axios"
-// const targetURL = "https://tic-tac-toe-by-vinay2812.vercel.app"
-const targetURL = "http://localhost:5000"
+const targetURL = "https://tic-tac-toe-by-vinay2812.up.railway.app"
+// const targetURL = "http://localhost:5000"
 export const API = axios.create({baseURL: targetURL});;
--- client/src/socketio.js
+++ client/src/socketio.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 export const getSocket = () =>{
-    const socket = io("http://localhost:5000");
+    const socket = io("https://tic-tac-toe-by-vinay2812.up.railway.app");
     return socket;
 }
