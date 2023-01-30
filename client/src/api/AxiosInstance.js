180d47e446ce08bbad43c594ecd96035cea1982d
Vinay <vinaysarda2812@gmail.com>
2023-01-30 21:08:18 +0530

    client api changed

--- client/src/api/AxiosInstance.js
+++ client/src/api/AxiosInstance.js
@@ -1,4 +1,5 @@
 import axios from "axios"
-const targetURL = "https://tic-tac-toe-by-vinay2812.up.railway.app"
+// const targetURL = "https://tic-tac-toe-by-vinay2812.up.railway.app"
 // const targetURL = "http://localhost:5000"
+const targetURL = "https://tic-tac-toe-by-vinay2812.vercel.app/"
 export const API = axios.create({baseURL: targetURL});;
