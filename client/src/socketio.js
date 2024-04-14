239a847ff304083d1e67a2caaaace7c9c480f906
Vinay2812 <vinaysarda2812@gmail.com>
2024-04-14 14:44:43 +0530

    ref: update socket.io url

--- client/src/socketio.js
+++ client/src/socketio.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 export const getSocket = () =>{
-    const socket = io("https://tic-tac-toe-by-vinay2812.up.railway.app");
+    const socket = io("https://tic-tac-toe-ubgb.onrender.com");
     return socket;
 }
